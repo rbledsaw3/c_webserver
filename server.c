a3ae525857ce4503f5fb33a14aa39938cb942269
Robert Bledsaw III <rbledsaw3@outlook.com>
2023-11-26 17:48:26 -0500

    linting and fixing some casts

--- server.c
+++ server.c
@@ -5,27 +5,34 @@
 #include <unistd.h>
 #include <netinet/in.h>
 
+enum {
+    PORT = 8080,
+    MAX_CONNECTIONS = 10,
+    BUFFER_SIZE = 256,
+    PADDING_SIZE = 5
+};
+
 int main() {
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in addr = {
         .sin_family = AF_INET,
-        .sin_port = htons(8080),
+        .sin_port = htons(PORT),
         .sin_addr.s_addr = INADDR_ANY
     };
 
-    bind(sock, (struct sockaddr *)&addr, sizeof(addr));
-    listen(sock, 10);
+    (void) bind(sock, (struct sockaddr *)&addr, sizeof(addr));
+    listen(sock, MAX_CONNECTIONS);
     
     int client_fd = accept(sock, 0, 0);  // file descriptor of client socket
 
-    char buffer[256] = {0};
-    recv(client_fd, buffer, 256, 0);
+    char buffer[BUFFER_SIZE] = {0};
+    recv(client_fd, buffer, BUFFER_SIZE, 0);
 
     // GET /index.html
-    char *f = buffer + 5;
-    *strchr(f, ' ') = 0;
-    int opened_fd = open(f, O_RDONLY);   // opened file descriptor
-    sendfile(client_fd, opened_fd, 0, 256);
+    char *file = buffer + PADDING_SIZE;
+    *strchr(file, ' ') = 0;
+    int opened_fd = open(file, O_RDONLY);   // opened file descriptor
+    sendfile(client_fd, opened_fd, 0, BUFFER_SIZE);
     close(opened_fd);
     close(sock);
 
