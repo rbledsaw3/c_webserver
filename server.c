7029d4e17b618a0ce7204a9df8a0791d23c9a671
Robert Bledsaw III <rbledsaw3@outlook.com>
2023-11-29 19:48:05 -0500

    added clang-format

--- server.c
+++ server.c
@@ -1,40 +1,31 @@
-#include <sys/socket.h>
-#include <string.h>
 #include <fcntl.h>
-#include <sys/sendfile.h>
-#include <unistd.h>
 #include <netinet/in.h>
+#include <string.h>
+#include <sys/sendfile.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
-enum {
-    PORT = 8080,
-    MAX_CONNECTIONS = 10,
-    BUFFER_SIZE = 256,
-    PADDING_SIZE = 5
-};
+enum { PORT = 8080, MAX_CONNECTIONS = 10, BUFFER_SIZE = 256, PADDING_SIZE = 5 };
 
 int main() {
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in addr = {
-        .sin_family = AF_INET,
-        .sin_port = htons(PORT),
-        .sin_addr.s_addr = INADDR_ANY
-    };
+  int sock = socket(AF_INET, SOCK_STREAM, 0);
+  struct sockaddr_in addr = { .sin_family = AF_INET, .sin_port = htons(PORT), .sin_addr.s_addr = INADDR_ANY };
 
-    (void) bind(sock, (struct sockaddr *)&addr, sizeof(addr));
-    listen(sock, MAX_CONNECTIONS);
-    
-    int client_fd = accept(sock, 0, 0);  // file descriptor of client socket
+  (void) bind(sock, (struct sockaddr*) &addr, sizeof(addr));
+  listen(sock, MAX_CONNECTIONS);
 
-    char buffer[BUFFER_SIZE] = {0};
-    recv(client_fd, buffer, BUFFER_SIZE, 0);
+  int client_fd = accept(sock, 0, 0);  // file descriptor of client socket
 
-    // GET /index.html
-    char *file = buffer + PADDING_SIZE;
-    *strchr(file, ' ') = 0;
-    int opened_fd = open(file, O_RDONLY);   // opened file descriptor
-    sendfile(client_fd, opened_fd, 0, BUFFER_SIZE);
-    close(opened_fd);
-    close(sock);
+  char buffer[BUFFER_SIZE] = { 0 };
+  recv(client_fd, buffer, BUFFER_SIZE, 0);
 
-    return 0;
+  // GET /index.html
+  char* file = buffer + PADDING_SIZE;
+  *strchr(file, ' ') = 0;
+  int opened_fd = open(file, O_RDONLY);  // opened file descriptor
+  sendfile(client_fd, opened_fd, 0, BUFFER_SIZE);
+  close(opened_fd);
+  close(sock);
+
+  return 0;
 }
